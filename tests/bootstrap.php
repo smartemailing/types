26ddf7b2c94108a6f06be30014a79afadc814f30
martin <martin@martinstrouhal.cz>
2018-06-24 19:55:40 +0200

    devel

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,11 @@ declare(strict_types = 1);
 
 namespace SmartEmailing\Types;
 
+use Tester\Environment;
+
 \error_reporting(\E_ALL);
 
 require __DIR__ . '/../vendor/autoload.php';
+
+Environment::setup();
+
