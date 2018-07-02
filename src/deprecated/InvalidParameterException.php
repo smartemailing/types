a3a2122e9749bc4e346d946c03ac5c820a78ef96
Martin Strouhal <martin@Martin-MacBook-Pro.local>
2018-07-02 13:47:35 +0200

    added deprecated InvalidParameterException to preserve BC

---
+++ src/deprecated/InvalidParameterException.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace SmartSelling\Parameters;
+
+/*
+ *
+ * All contents of this file are deprecated and will be removed in the future
+ *
+ *
+ */
+
+/** @noinspection ClassConstantCanBeUsedInspection */
+
+use Consistence\Type\ObjectMixinTrait;
+
+if (!\class_exists('SmartSelling\Parameters\InvalidParameterException')) {
+	/**
+	 * Class InvalidParameterException
+     *
+     * @package SmartSelling\Parameters
+	 * @deprecated
+	 */
+	abstract class InvalidParameterException extends \RuntimeException
+	{
+
+		use ObjectMixinTrait;
+
+	}
+}
+
