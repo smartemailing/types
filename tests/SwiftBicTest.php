067fa8bf7262f6154e02c09706da1dd089005805
Jiří Slischka <slischkaj@gmail.com>
2018-07-03 23:39:45 +0200

    Added Iban and SwiftBic

---
+++ tests/SwiftBicTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace SmartEmailing\Types;
+
+use Tester\Assert;
+use Tester\TestCase;
+
+require_once __DIR__ . '/bootstrap.php';
+
+final class SwiftBicTest extends TestCase
+{
+
+	public function testValidIban(): void
+	{
+		foreach ($this->getValidIbanValues() as $validIbanValue) {
+			$iban = SwiftBic::from($validIbanValue);
+			Assert::type(SwiftBic::class, $iban);
+			Assert::type('string', $iban->getValue());
+		}
+	}
+
+	public function testInvalidIban(): void
+	{
+		foreach ($this->getInvalidIbanValues() as $invalidIbanValue) {
+			Assert::exception(function () use ($invalidIbanValue): void {
+				SwiftBic::from($invalidIbanValue);
+			}, InvalidTypeException::class);
+		}
+	}
+
+	/**
+	 * @return string[]
+	 */
+	private function getValidIbanValues(): array
+	{
+		return [
+			'GIBACZPX',
+		];
+	}
+
+	/**
+	 * @return string[]
+	 */
+	private function getInvalidIbanValues(): array
+	{
+		return [
+			'GIBACZP',
+			'IBACZPX',
+		];
+	}
+
+}
+
+(new SwiftBicTest())->run();
