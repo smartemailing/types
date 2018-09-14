b9049d649ab37f3ebd0496d5f25327545cd34982
Martin Strouhal <martin@martinstrouhal.cz>
2018-09-14 20:26:16 +0200

    removed deprecated aliases

--- src/deprecated/Currency.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types = 1);
-
-namespace SmartEmailing\Types\deprecated;
-
-use SmartEmailing\Types\CurrencyCode;
-
-\class_alias(CurrencyCode::class, '\SmartEmailing\Types\Currency', true);
