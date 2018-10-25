c079d84affe0501d8fb626f0ee6f7374e7955a5e
Martin Strouhal <martin@martinstrouhal.cz>
2018-10-25 23:56:48 +0200

    cleanup

---
+++ src/Helpers/InvisibleSpaceCharacterCodes.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace SmartEmailing\Types\Helpers;
+
+abstract class InvisibleSpaceCharacterCodes
+{
+
+	/**
+	 * @var int[]
+	 */
+	private static $codes = [
+		0x9,
+		0xA,
+		0xB,
+		0xC,
+		0xD,
+		0x20,
+		0x85,
+		0xA0,
+		0x1680,
+		0x2000,
+		0x2001,
+		0x2002,
+		0x2003,
+		0x2004,
+		0x2005,
+		0x2006,
+		0x2007,
+		0x2008,
+		0x2009,
+		0x200A,
+		0x2028,
+		0x2029,
+		0x202F,
+		0x205F,
+		0x3000,
+		0xAD,
+		0xF0,
+		0xC2AD,
+		0xCA,
+		0xC2,
+	];
+
+	/**
+	 * @return int[]
+	 */
+	final public static function getCodes(): array
+	{
+		return self::$codes;
+	}
+
+}
